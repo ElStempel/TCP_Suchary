81624712f1ba29af79a0ac9a58a69947a083ad88
ElStempel <steplewski.aleks@gmail.com>
2020-10-13 23:14:00 +0200

    Początkowe zaewidencjonowanie modułu TCPserver

---
+++ TCPserver/Joke.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace TCPserver
+{
+    /// <summary>
+    /// Klasa Joke - przechowuje i wybiera suchary.
+    /// </summary>
+    public class Joke
+    {
+
+        private string[] suchary = new string[5];
+        
+        public Joke()
+        {
+            suchary[0] = "\nGdzie podpisano traktat wersalski? \n - Na samym dole, pod tekstem.";
+            suchary[1] = "\n- Co sie po jednej stronie glaszcze a po drugiej lize?? \n- Nie wiem. \n-... znaczek pocztowy!!";
+            suchary[2] = "\nCo sie stanie jak walec drogowy przejedzie czlowieka? \n- Konwersja obrazu z 3D na 2D";
+            suchary[3] = "\nJak sie nazywa mnich odpowiedzialny za podatki? \n- Brat PIT.";
+            suchary[4] = "\n-Jaka jest ulubiona zabawa dzieci grabarza?  \n- W chowanego.";
+        }
+
+
+        /// <summary>
+        /// Metoda wybierająca suchary z puli.
+        /// </summary>
+        /// <returns></returns>
+        public string genJoke()
+        {
+            Random rnd = new Random();
+            int indeks = rnd.Next(0, suchary.Length);
+            string suchar = suchary[indeks];
+
+            return suchar;
+        }
+    }
+}
---
+++ TCPserver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPserver
+{
+    /// <summary>
+    /// Klasa Program serwera TCP
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8008);
+            server.Start();
+            Console.Write("Odpalam server\n");
+            string negatyw = "Ja tylko serwuje suchary\n";
+            byte[] bytes = Encoding.ASCII.GetBytes(negatyw);
+            Joke suchy = new Joke();
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.Write("Polaczono\n");
+                byte[] buffer = new byte[1024];
+                client.GetStream().Read(buffer);
+                string text = Encoding.UTF8.GetString(buffer).Replace("\n", "").Replace("\0", "");
+                Console.Write(text);
+                if (check(text) == true)
+                {
+                    Console.Write("Potwierdzam\n");
+                    String sucharek = suchy.genJoke();
+                    byte[] pozytyw = Encoding.ASCII.GetBytes(sucharek);
+                    client.GetStream().Write(pozytyw, 0, pozytyw.Length);
+
+                } else
+                {
+                    Console.Write("Odrzucam\n");
+                    client.GetStream().Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sprawdzanie czy text jest zgodny
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static bool check(String text)
+        {
+            if(text == "suchar")
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+            
+        }
+
+    }
+}
